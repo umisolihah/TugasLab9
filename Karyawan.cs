412c32cfdce1eb36c824374b2a442563796988bf
Umi Solihah <61912758+umisolihah@users.noreply.github.com>
2020-06-30 11:41:47 +0700

    Inisialisasi Project

---
+++ Karyawan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasLab9
+{
+    public abstract class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public string JK { get; set; }
+        public abstract double Gaji();
+    }
+}
